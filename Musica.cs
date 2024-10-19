f76b01862af6e208aabe349a2fd6a9234f08ffad
Cleber-0101 <cleberbatista99@hotmail.com>
2024-10-18 23:12:47 -0300

    Paradigma de programação Orientado a Objetos

---
+++ Musica.cs
@@ -0,0 +1,42 @@
+﻿
+using System.ComponentModel.Design;
+
+class Musica
+{
+    public string NomedaMusca { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+    public bool Possuiplano { get; set; }
+
+
+    //Atribuindo somente o get tomamos o controle do codigo fazendo com que não consigam Atribuir valores a esse atributo 
+    public string DescricaoResumida {
+        get
+        {
+            return $"A musica {NomedaMusca} pertence a banda {Artista}";
+        }
+    }
+
+
+    //corportamento de uma musica criando um metodo
+    public void ExibirfichaTecnica()
+    {
+        Console.WriteLine($"Nome: {NomedaMusca}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao} " + " min");
+
+
+        if (Disponivel)
+        {
+            Console.WriteLine($"A musica: ({NomedaMusca}) esta disponivel para dowload e em todas plataforma digitais");
+        }
+         if(Disponivel && Possuiplano)
+        {
+            Console.WriteLine("Livre acesso pra voce");
+        }else
+        {
+            Console.WriteLine("voce não tem acesso a esta musica ");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿////CONCEITO DE MUSICA
+
+////o que uma musica tem ?
+////Nome 
+////artista
+////duração
+////disponivel
+
+
+//Criando uma musica de acordo com a classe de criação aonde contem os moldes que toda musica tem 
+Musica  musica1 = new Musica();
+musica1.NomedaMusca = "È ele";
+musica1.Artista = "Banda Drop";
+musica1.Duracao = 10;
+musica1.Disponivel = true;
+musica1.Possuiplano = true;
+musica1.DescricaoResumida = "ddd";
+
+
+Musica musica2 = new Musica();
+musica2.NomedaMusca = "teste criação de musicas";
+musica2.Artista = "Banda teste";
+musica2.Duracao = 100;
+musica2.Disponivel = false;
+musica2.Possuiplano = true;
+
+musica1.ExibirfichaTecnica();
+musica2.ExibirfichaTecnica();
+
